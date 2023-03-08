f5c738c4b8fc4bc3c03280c66f29350560e0dded
hesham156 <h456ad@gmail.com>
2023-03-08 23:46:18 +0200

    img

--- src/App.js
+++ src/App.js
@@ -21,22 +21,32 @@ function App() {
 
   const lnks= [{url:'/',name:data[1]?.lnks[0]},{url:'/about',name:data[1]?.lnks[1]},{url:'/Servie',name:data[1]?.lnks[2]}]
 
- const imgs =['https://media.lastingdynamics.com/wp-content/uploads/2021/10/IBM-Logo-White.svg',
- 'https://media.lastingdynamics.com/wp-content/uploads/2021/10/logo-customer-yoozen-white.png',
- 'https://media.lastingdynamics.com/wp-content/uploads/2021/10/Sankaku-Complex-logo-white.svg',
- 'https://media.lastingdynamics.com/wp-content/uploads/2021/10/IBM-Logo-White.svg',
- 'https://media.lastingdynamics.com/wp-content/uploads/2021/10/logo-customer-yoozen-white.png',
- 'https://media.lastingdynamics.com/wp-content/uploads/2021/10/Sankaku-Complex-logo-white.svg',
- 'https://media.lastingdynamics.com/wp-content/uploads/2021/10/IBM-Logo-White.svg',
- 'https://media.lastingdynamics.com/wp-content/uploads/2021/10/logo-customer-yoozen-white.png',
- 'https://media.lastingdynamics.com/wp-content/uploads/2021/10/Sankaku-Complex-logo-white.svg',
- 'https://media.lastingdynamics.com/wp-content/uploads/2021/10/IBM-Logo-White.svg',
- 'https://media.lastingdynamics.com/wp-content/uploads/2021/10/logo-customer-yoozen-white.png',
- 'https://media.lastingdynamics.com/wp-content/uploads/2021/10/Sankaku-Complex-logo-white.svg',
- 'https://media.lastingdynamics.com/wp-content/uploads/2021/10/Sankaku-Complex-logo-white.svg',
- 'https://media.lastingdynamics.com/wp-content/uploads/2021/10/Sankaku-Complex-logo-white.svg',
- 'https://media.lastingdynamics.com/wp-content/uploads/2021/10/Sankaku-Complex-logo-white.svg',
- 'https://media.lastingdynamics.com/wp-content/uploads/2021/10/Sankaku-Complex-logo-white.svg']
+ const imgs =['https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_84fd5bd582809f20ff5682d2746ffa82/shopify.png',
+ 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/WordPress_blue_logo.svg/1200px-WordPress_blue_logo.svg.png',
+ 'https://salla.dev/wp-content/uploads/2021/03/photo_2021-02-25-08.38.42-4.jpeg',
+ 'https://zid.sa/wp-content/uploads/2022/08/zid-logo-1.png','https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_84fd5bd582809f20ff5682d2746ffa82/shopify.png',
+ 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/WordPress_blue_logo.svg/1200px-WordPress_blue_logo.svg.png',
+ 'https://salla.dev/wp-content/uploads/2021/03/photo_2021-02-25-08.38.42-4.jpeg',
+ 'https://zid.sa/wp-content/uploads/2022/08/zid-logo-1.png','https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_84fd5bd582809f20ff5682d2746ffa82/shopify.png',
+ 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/WordPress_blue_logo.svg/1200px-WordPress_blue_logo.svg.png',
+ 'https://salla.dev/wp-content/uploads/2021/03/photo_2021-02-25-08.38.42-4.jpeg',
+ 'https://zid.sa/wp-content/uploads/2022/08/zid-logo-1.png','https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_84fd5bd582809f20ff5682d2746ffa82/shopify.png',
+ 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/WordPress_blue_logo.svg/1200px-WordPress_blue_logo.svg.png',
+ 'https://salla.dev/wp-content/uploads/2021/03/photo_2021-02-25-08.38.42-4.jpeg',
+ 'https://zid.sa/wp-content/uploads/2022/08/zid-logo-1.png','https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_84fd5bd582809f20ff5682d2746ffa82/shopify.png',
+ 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/WordPress_blue_logo.svg/1200px-WordPress_blue_logo.svg.png',
+ 'https://salla.dev/wp-content/uploads/2021/03/photo_2021-02-25-08.38.42-4.jpeg',
+ 'https://zid.sa/wp-content/uploads/2022/08/zid-logo-1.png','https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_84fd5bd582809f20ff5682d2746ffa82/shopify.png',
+ 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/WordPress_blue_logo.svg/1200px-WordPress_blue_logo.svg.png',
+ 'https://salla.dev/wp-content/uploads/2021/03/photo_2021-02-25-08.38.42-4.jpeg',
+ 'https://zid.sa/wp-content/uploads/2022/08/zid-logo-1.png','https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_84fd5bd582809f20ff5682d2746ffa82/shopify.png',
+ 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/WordPress_blue_logo.svg/1200px-WordPress_blue_logo.svg.png',
+ 'https://salla.dev/wp-content/uploads/2021/03/photo_2021-02-25-08.38.42-4.jpeg',
+ 'https://zid.sa/wp-content/uploads/2022/08/zid-logo-1.png','https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_84fd5bd582809f20ff5682d2746ffa82/shopify.png',
+ 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/WordPress_blue_logo.svg/1200px-WordPress_blue_logo.svg.png',
+ 'https://salla.dev/wp-content/uploads/2021/03/photo_2021-02-25-08.38.42-4.jpeg',
+ 'https://zid.sa/wp-content/uploads/2022/08/zid-logo-1.png'
+ ]
   return (
     <>
      <NavBar setLang={setLang} dir={lang==="ar"?"rtl":"ltr"} lnks={lnks} logo={"https://scontent.faly1-2.fna.fbcdn.net/v/t39.30808-1/332315756_215651317628878_2444922090375883785_n.jpg?stp=dst-jpg_p320x320&_nc_cat=104&ccb=1-7&_nc_sid=c6021c&_nc_eui2=AeG5gW4BXY4ieWNWCUtaEq5rJaaaIJMp7VMlppogkyntU0jsuc4cvHjWnbvh7p29230IP-CESyVkzdZRv9fJWO2G&_nc_ohc=DxKRJJ3ILbYAX-hcYia&_nc_ht=scontent.faly1-2.fna&oh=00_AfBlEx1q0tVKV3y7YNqFK7mqr-wZ9fppsZwZIxC0un1WqA&oe=640C80F6"} siteName={"MrRobot"}/>
--- src/Component/Imgani.js
+++ src/Component/Imgani.js
@@ -4,7 +4,7 @@ export const Imgani = ({imgs}) => {
   return (
     <div className='imgany logo-loop '>
         {imgs?.map((imgsrc)=>{
-            return <img src={imgsrc} alt="service"/>
+            return <img src={imgsrc} key={imgs.indexOf(imgsrc)} alt="service"/>
         })}
     </div>
   )
