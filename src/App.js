fae4d5bf1372b986a4493a1b2cbe4d4011a1287b
hesham156 <h456ad@gmail.com>
2023-03-13 00:44:07 +0200

    ani

--- src/App.js
+++ src/App.js
@@ -49,6 +49,59 @@ Aos.init()
      <NavBar  lnks={lnks} logo={"https://scontent.faly1-2.fna.fbcdn.net/v/t39.30808-1/332315756_215651317628878_2444922090375883785_n.jpg?stp=dst-jpg_p320x320&_nc_cat=104&ccb=1-7&_nc_sid=c6021c&_nc_eui2=AeG5gW4BXY4ieWNWCUtaEq5rJaaaIJMp7VMlppogkyntU0jsuc4cvHjWnbvh7p29230IP-CESyVkzdZRv9fJWO2G&_nc_ohc=DxKRJJ3ILbYAX-hcYia&_nc_ht=scontent.faly1-2.fna&oh=00_AfBlEx1q0tVKV3y7YNqFK7mqr-wZ9fppsZwZIxC0un1WqA&oe=640C80F6"} siteName={"MrRobot"}/>
      <Header/>
      <Service/>
+     <div className="stars">
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+  <div className="star"></div>
+</div>
+
      {/* <ServiceCard/> */}
      {/* <section data-aos="fade-up">
       <div className='container'>
